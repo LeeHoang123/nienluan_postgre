16f8f1cd1ef21d919ce90feabac23383738a4055
LeeHoang123 <128125195+LeeHoang123@users.noreply.github.com>
2024-10-16 21:27:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ app.put('/api/users/:username', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server đang chạy tại http://localhost:${PORT}/`);
 });
